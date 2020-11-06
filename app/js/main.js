d88e93cc6b8ca7decbfaa8fcae2c3d671dbbff3c
licler <ute3@yandex.ru>
2020-11-06 22:21:29 +0300

    added product, week

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,30 @@
 $(function(){
 
 
+      $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '14px'
+      });
 
-    
+
+     
+      $('.week__inner-items').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+  var mixer = mixitup('.product__inner-items');   
 });
