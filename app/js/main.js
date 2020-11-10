bb46a125873d9ab5ab3ad40188b33d17708e4f2a
licler <ute3@yandex.ru>
2020-11-10 20:34:48 +0300

    range

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,15 @@ $(function(){
         arrows: false,
         dots: true
       });
+      
+      $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500,
+        grid: true
+    });
 
 
 
