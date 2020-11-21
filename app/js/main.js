562985844e9ff7cb9b4d19683215c175311753bb
licler <ute3@yandex.ru>
2020-11-21 19:59:58 +0300

    start tabs

--- app/js/main.js
+++ app/js/main.js
@@ -36,7 +36,14 @@ $(function(){
       $('.icon-list').removeClass('active');
     });
 
-    
+    $('.single-page__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.single-page__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.single-page__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
 
 
